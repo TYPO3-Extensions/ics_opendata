b1f2daef94636fd13e23a96baa2174f4fde7d7f9
mygoddess <mygoddess@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-14 14:00:35 +0000

    Renamed eID from ics_od_core_api to ics_od_api. Because people are lazy and a shorter name is better for the memory. And its uniqueness is the same.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_opendata/trunk@48705 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_od_core_api/ext_localconf.php
+++ ics_od_core_api/ext_localconf.php
@@ -6,5 +6,5 @@ if (!defined ('TYPO3_MODE')) {
  	die ('Access denied.');
 }
 
-$TYPO3_CONF_VARS['FE']['eID_include']['ics_od_core_api'] = 'EXT:ics_od_core_api/api/tx_icsodcoreapi_client.php';
+$TYPO3_CONF_VARS['FE']['eID_include']['ics_od_api'] = 'EXT:ics_od_core_api/api/tx_icsodcoreapi_client.php';
 ?>
