49e844d8cdd5d42f6f7895270a34ef3676d8fffb
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-22 10:05:33 +0000

    Tags ics_od_appstore version 1.0.2. Tags ics_od_categories version 1.0.1. Tags ics_od_core_api version 1.0.2. Tags ics_od_datastore version 1.0.1. Tags ics_od_stores_rel version 1.0.1. Reorganize first version tags.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_opendata/tags@59515 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ics_od_core_api/1.0.2/ext_localconf.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * $Id$
+ */
+if (!defined ('TYPO3_MODE')) {
+ 	die ('Access denied.');
+}
+
+$TYPO3_CONF_VARS['FE']['eID_include']['ics_od_api'] = 'EXT:ics_od_core_api/api/tx_icsodcoreapi_client.php';
+?>
