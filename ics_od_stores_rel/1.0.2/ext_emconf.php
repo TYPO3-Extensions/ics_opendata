1595c52a0c3f65656e6c5efbd6c71f1e0adf0936
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-25 14:00:06 +0000

    Tags ics_od_stores_rel version 1.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_opendata/tags@61111 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ics_od_stores_rel/1.0.2/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ics_od_stores_rel".
+#
+# Auto generated 23-03-2012 16:05
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Opendata Appstore Datastore Relation',
+	'description' => 'The relation between opendata datastore and appstore',
+	'category' => 'plugin',
+	'author' => 'In Cite Solution',
+	'author_email' => 'technique@in-cite.net',
+	'shy' => '',
+	'dependencies' => 'ics_od_appstore,ics_od_datastore',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.2',
+	'constraints' => array(
+		'depends' => array(
+			'ics_od_appstore' => '',
+			'ics_od_datastore' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"da92";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"1789";s:12:"ext_icon.gif";s:4:"2505";s:17:"ext_localconf.php";s:4:"2a3b";s:14:"ext_tables.php";s:4:"00a3";s:14:"ext_tables.sql";s:4:"a8da";s:24:"ext_typoscript_setup.txt";s:4:"7942";s:13:"locallang.xml";s:4:"23d9";s:16:"locallang_db.xml";s:4:"7da2";s:14:"doc/manual.sxw";s:4:"1f52";s:19:"doc/wizard_form.dat";s:4:"dba7";s:20:"doc/wizard_form.html";s:4:"cd20";s:45:"hook/class.tx_icsodstoresrel_appsDatasets.php";s:4:"2ec1";s:18:"hook/locallang.xml";s:4:"23d9";s:17:"res/template.html";s:4:"64bd";}',
+	'suggests' => array(
+	),
+);
+
+?>
