e85fa9be0b5268b02b2ede34a4c24f5f2a79f6cf
mygoddess <mygoddess@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-02 14:27:27 +0000

    Bug fix: Missing is_object test before get_class usage in xml2json.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_opendata/trunk@81886 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_od_core_api/lib/xml2json/xml2json.php
+++ ics_od_core_api/lib/xml2json/xml2json.php
@@ -171,7 +171,7 @@ class xml2json {
 		}
 
 		if ($recursionDepth == 0) {
-			if (get_class($simpleXmlElementObject) != SIMPLE_XML_ELEMENT_PHP_CLASS) {
+			if (!is_object($simpleXmlElementObject) || (get_class($simpleXmlElementObject) != SIMPLE_XML_ELEMENT_PHP_CLASS)) {
 				// If the external caller doesn't call this function initially  
 				// with a SimpleXMLElement object, return now.				
 				return(null);				
@@ -182,7 +182,7 @@ class xml2json {
 			}
 		} // End of if ($recursionDepth == 0) {		
 				
-		if (get_class($simpleXmlElementObject) == SIMPLE_XML_ELEMENT_PHP_CLASS) {
+		if (is_object($simpleXmlElementObject) && get_class($simpleXmlElementObject) == SIMPLE_XML_ELEMENT_PHP_CLASS) {
 			// Get a copy of the simpleXmlElementObject
 			$copyOfsimpleXmlElementObject = $simpleXmlElementObject;
       		// Get the object variables in the SimpleXmlElement object for us to iterate.
